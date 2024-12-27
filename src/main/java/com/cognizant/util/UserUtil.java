658a00f9a6fb2b1a994bddbfe26b9c16c44c494c
kumarsolanki <59783616+kumarsolanki@users.noreply.github.com>
2024-12-28 00:02:33 +0530

    Update UserUtil.java

--- src/main/java/com/cognizant/util/UserUtil.java
+++ src/main/java/com/cognizant/util/UserUtil.java
@@ -8,8 +8,8 @@ public class UserUtil {
     private static List<User> users = new ArrayList<>();
     public static List<User> getInitialUsers() {
         if (users.isEmpty()) {
-            users.add(new User("user1", "user1@example.com", "1234567890"));
-            users.add(new User("user2", "user2@example.com", "0987654321"));
+            users.add(new User("user1", "user1@gmail.com", "1234567890"));
+            users.add(new User("user2", "user2@gmail.com", "0987654321"));
         }
         return users;
     }
